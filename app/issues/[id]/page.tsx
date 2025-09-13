98b8bb094ec633c69a7170ed6c7e018cb094d045
Lawrence Moore <thelawrencemoore@gmail.com>
2025-09-13 13:18:56 -0700

    Added await to params to suppress NextJS error

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,10 @@
 import IssueDetailPage from "@/components/custom/issues/IssueDetailPage";
 
 interface PageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
-export default function Page({ params }: PageProps) {
-  const { id } = params;
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
   return <IssueDetailPage issueId={id ?? ""} />;
 }
